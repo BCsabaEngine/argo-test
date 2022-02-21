456e5a3eb23237e04d52e435b7e30f536b623b48
Balázs Csaba <csaba@balazs-magyar.hu>
2022-02-21 09:52:22 +0100

    update

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const K8S_POD_IP = env.get('K8S_POD_IP').default('').asString();
 const K8S_POD_SERVICE_ACCOUNT = env.get('K8S_POD_SERVICE_ACCOUNT').default('').asString();
 
 const requestListener = function (req, res) {
-    setTimeout(() => {
+//    setTimeout(() => {
         res.writeHead(200).end(`[${version}] (${K8S_NODE_NAME}/${K8S_POD_NAME}/${K8S_POD_NAMESPACE}/${K8S_POD_IP}/${K8S_POD_SERVICE_ACCOUNT}): Now is: ` + new Date().toISOString());
-    }, 20);
+//    }, 20);
 }
 
 const server = http.createServer(requestListener);
